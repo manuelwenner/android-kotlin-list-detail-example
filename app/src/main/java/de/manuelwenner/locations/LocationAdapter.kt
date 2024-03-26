fedfc697e695af33b89729fb5d280462cc1fcd91
Manuel Wenner <hi@manuelwenner.de>
2024-03-26 15:35:05 +0100

    feat: code refactoring

--- app/src/main/java/de/manuelwenner/locations/LocationAdapter.kt
+++ app/src/main/java/de/manuelwenner/locations/LocationAdapter.kt
@@ -1,7 +1,5 @@
 package de.manuelwenner.locations
 
-import android.widget.ListAdapter
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/de/manuelwenner/locations/MainActivity.kt
+++ app/src/main/java/de/manuelwenner/locations/MainActivity.kt
@@ -3,12 +3,8 @@ package de.manuelwenner.locations
 import android.content.Intent
 import android.os.Bundle
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.ListView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var locationListView: ListView
@@ -16,13 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         locationListView = findViewById(R.id.locationListView)
 
